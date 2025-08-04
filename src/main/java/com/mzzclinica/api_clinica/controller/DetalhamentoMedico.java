44fc82bc07c78aba74454aecbb1b8bf83303645d
Mzdudul <duduzim.gon05@gmail.com>
2025-08-04 10:16:45 -0300

    implementando boas praticas nos metodos do pacote controller

---
+++ src/main/java/com/mzzclinica/api_clinica/controller/DetalhamentoMedico.java
@@ -0,0 +1,13 @@
+package com.mzzclinica.api_clinica.controller;
+
+import com.mzzclinica.api_clinica.endereco.Endereco;
+import com.mzzclinica.api_clinica.medico.Especialidade;
+import com.mzzclinica.api_clinica.medico.Medico;
+
+public record DetalhamentoMedico(Long id, String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
+
+    public DetalhamentoMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade(), medico.getEndereco());
+    }
+
+}
--- src/main/java/com/mzzclinica/api_clinica/controller/MedicoController.java
+++ src/main/java/com/mzzclinica/api_clinica/controller/MedicoController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.mzzclinica.api_clinica.medico.DadosMedico;
 import com.mzzclinica.api_clinica.medico.Medico;
@@ -33,26 +35,40 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrarMedico(@RequestBody @Valid DadosMedico dadosMedico) {
+    public ResponseEntity cadastrarMedico(@RequestBody @Valid DadosMedico dadosMedico, UriComponentsBuilder uriBuilder) {
+        var medico = new Medico(dadosMedico);
+        repository.save(medico);
+        
         repository.save(new Medico(dadosMedico));
+
+        var uri = uriBuilder.path("/medicos/{id}")
+                .buildAndExpand(medico.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(new DetalhamentoMedico(medico));
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page);
     }
     
     @PutMapping()
     @Transactional
-    public void atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dadosMedico) {
+    public ResponseEntity atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedico dadosMedico) {
        var medico = repository.getReferenceById(dadosMedico.id());
        medico.atualizarInformacoes(dadosMedico);
+
+       return ResponseEntity.ok(new DetalhamentoMedico(medico));
 
     }
 
     @DeleteMapping("/{id}")
-    public void excluirMedico(@PathVariable Long id) {
+    public ResponseEntity excluirMedico(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 }
